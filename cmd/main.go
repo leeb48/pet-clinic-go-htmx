dd4b16be0ca1e3a189370f9bbdc4a9b47cc02db7
bong <bongsoolee91@gmail.com>
2023-12-31 02:45:50 -0800

    connected to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -19,6 +22,13 @@ func main() {
 		AddSource: true,
 		Level:     slog.LevelDebug,
 	}))
+
+	db, err := openDB("app:1234@/petClinic?parseTime=true")
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	templateCache, err := createTemplateCache()
 	if err != nil {
@@ -35,4 +45,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pet-clinic.bonglee.com
 
 go 1.21.4
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
